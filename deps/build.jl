e492e906008489654d2142b1890284c06631dfb4
ozanarkancan <can.ozanarkan@gmail.com>
2018-03-06 14:11:23 +0300

    update build

--- deps/build.jl
+++ deps/build.jl
@@ -12,14 +12,40 @@ function install_envs()
         info("To install everything, set ENV[\"GYM_ENVS\"]=all")
         info("Then build the package again, Pkg.build(\"Gym\")")
     end
+    cd("..")
+end
+
+function which_gym()
+    whichgym = ""
+    filename = "whichgym"
+    if isfile(filename)
+        f = open(whichgym)
+        whichgym = strip(readstring(f))
+        close(f)
+    else
+        whichgym = "SYSTEM"
+    end
+    return whichgym
 end
 
 try
     using PyCall
     PyCall.pyimport("gym")
-    install_envs()
+    if which_gym() == "SYSTEM"
+        warn("You are using the gym from the systemwise installation.")
+        warn("If you want to install new environments, you should install them using \"pip install -e .[env]\"")
+    else
+        install_envs()
+    end
 catch
-    info("OpenAi gym will be installed")
-    run(`git clone https://github.com/openai/gym.git`)
+    if !isdir("gym")
+        info("Downloading OpenAi gym")
+        run(`git clone https://github.com/openai/gym.git`)
+    end
+    info("Installing OpenAi gym")
     install_envs()
+    filename = "whichgym"
+    f = open(filename, "w")
+    write(f, "OWN")
+    close(f)
 end
